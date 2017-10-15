6f7371a2590be6950dcb41531f0b21b072e506ce
io <webworkk@mail.ru>
2017-10-15 22:57:15 +0300

    initial commit

---
+++ Modules/VariableParser/src/com/ivo/Main.java
@@ -0,0 +1,14 @@
+package com.ivo;
+
+/**
+ *
+ * @author IOAdmin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String in = "(x1 - 2) * (x1 - 2) + (x2 - 1) * (x2 - 1)";
+    }
+    
+    
+}
