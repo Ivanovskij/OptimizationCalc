fbc6c55bbc3f7ec0d27ff2a779a871ced247df15
io <webworkk@mail.ru>
2017-10-16 22:07:48 +0300

    start write parser for lexer

--- Modules/VariableParser/src/com/ivo/parser/Parser.java
+++ Modules/VariableParser/src/com/ivo/parser/Parser.java
@@ -1,9 +1,31 @@
 package com.ivo.parser;
 
+import java.util.List;
+
 /**
  *
  * @author IOAdmin
  */
 public class Parser {
 
+    private final Token EOF = new Token(TokenType.EOF, "");
+    
+    private List<Token> tokens;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+    
+    public double parse() {
+        while (!match(TokenType.EOF)) {
+            
+        }
+        
+        return 0;
+    }
+
+    private boolean match(TokenType tokenType) {
+        return false;
+    }
+    
 }
--- Modules/VariableParser/src/com/ivo/parser/TokenType.java
+++ Modules/VariableParser/src/com/ivo/parser/TokenType.java
@@ -22,4 +22,6 @@ public enum TokenType {
     LPAREN,
     RPAREN,
     
+    EOF
+    
 }
