3269aced421eb3f90c1c5854e735ae83c9078c22
io <webworkk@mail.ru>
2017-10-16 13:51:38 +0300

    start writing simplex method module

---
+++ Modules/SimplexMethod/src/com/ivo/module/SimplexMethod.java
@@ -0,0 +1,39 @@
+package com.ivo.module;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author IOAdmin
+ */
+public class SimplexMethod {
+
+    // таблица без базисных переменных
+    private double[][] table;
+    
+    // размерность строки, столбца
+    private int n, m;
+    
+    // базисы
+    private List<Integer> basis;
+
+    public SimplexMethod(double[][] source) {
+        n = source.length;
+        m = source.length;
+        
+        table = new double[n][m];
+        basis = new ArrayList<>();
+        
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                table[row][col] = source[row][col];
+            }
+        }
+    }
+    
+    
+
+    
+    
+}
