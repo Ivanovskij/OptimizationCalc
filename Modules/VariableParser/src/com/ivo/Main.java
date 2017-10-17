4a3921fcc64b3f2e393b3dad4a00097ee111f14b
io <webworkk@mail.ru>
2017-10-17 12:02:29 +0300

    wrote parsing for variables

--- Modules/VariableParser/src/com/ivo/Main.java
+++ Modules/VariableParser/src/com/ivo/Main.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        String in = "3 + 5";
+        String in = "21*x1 + 18*x2 + 16*x3 + 17.5*x4";
 
         Lexer lexer = new Lexer(in);
         List<Token> tokens = lexer.parse();
@@ -21,7 +21,13 @@ public class Main {
             System.out.println(token);
         }
         
-        List<Expression> expressions = new Parser(tokens, null).parse();
+        double x1 = 1;
+        double x2 = 2;
+        double x3 = 3;
+        double x4 = 4;
+        
+        List<Expression> expressions = new Parser(tokens, 
+                x1, x2, x3, x4).parse();
         for (Expression expr : expressions) {
             System.out.println(expr + " = " + expr.eval());
         }
--- Modules/VariableParser/src/com/ivo/parser/Parser.java
+++ Modules/VariableParser/src/com/ivo/parser/Parser.java
@@ -15,13 +15,16 @@ public class Parser {
     private final Token EOF = new Token(TokenType.EOF, "");
     
     private final List<Token> tokens;
+    private final Double[] args;
     
     private final int size;
     private int pos;
 
-    public Parser(List<Token> tokens, Double[] args) {
+    public Parser(List<Token> tokens, Double... args) {
         this.tokens = tokens;
         size = tokens.size();
+        
+        this.args = args;
         
         pos = 0;
     }
@@ -92,8 +95,38 @@ public class Parser {
         final Token current = get(0);
         if (match(TokenType.NUMBER)) {
             return new NumberExpression(Double.parseDouble(current.getText()));
+        } else if (match(TokenType.WORD)) {
+            int variableCounter = variablesCount();
+            if (variableCounter != args.length) {
+                throw new RuntimeException("Expected " + variableCounter + " arguments!");
+            }
+
+            setVariableValueByArg();
+            
+            return new NumberExpression(Double.parseDouble(current.getText()));
         }
         throw new RuntimeException("Unknown expression!");
+    }
+
+    private void setVariableValueByArg() {
+        int argCounter = 0;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens.get(i).getTokenType() == TokenType.WORD) {
+                tokens.get(i).setText(String.valueOf(args[argCounter]));
+                argCounter++;
+            }
+        }
+    }
+    
+    private int variablesCount() {
+        int variableCounter = 0;
+        for (Token token : tokens) {
+            if (token.getTokenType() == TokenType.WORD) {
+                variableCounter++;
+            }
+        }
+
+        return variableCounter;
     }
 
     
