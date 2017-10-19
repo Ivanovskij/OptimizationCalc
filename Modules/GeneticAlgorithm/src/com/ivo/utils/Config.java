d645beb6bf412f408c6fa9c66de7673f1924cd35
io <webworkk@mail.ru>
2017-10-19 16:44:05 +0300

    add util config

---
+++ Modules/GeneticAlgorithm/src/com/ivo/utils/Config.java
@@ -0,0 +1,17 @@
+package com.ivo.utils;
+
+/**
+ *
+ * @author IOAdmin
+ */
+public class Config {
+
+    private int max_generation;
+    private int population_count;
+    private int gene_size;
+    private int mutation_likelihood;
+    
+    
+    
+    
+}
