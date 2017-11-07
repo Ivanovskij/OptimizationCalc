2bc232073c3e39ebedce0097b7a4a30801c69a8b
io <webworkk@mail.ru>
2017-11-07 16:22:56 +0300

    wrote simplex module

--- Modules/SimplexMethod/src/com/ivo/MainSimplex.java
+++ Modules/SimplexMethod/src/com/ivo/MainSimplex.java
@@ -1,9 +1,29 @@
 package com.ivo;
 
+import com.ivo.module.SimplexMethod;
+
 /**
  *
  * @author IOAdmin
  */
 public class MainSimplex {
 
+    public static void main(String[] args) {
+        double[][] table = { {22, 8,  7, 5, 9},
+                                {25, 8,  9, 7, 8},
+                                {38,  10,  9, 9, 7},
+                                { 30,  10, 11, 11, 6},
+                                { 0, -21, -18, -16, -17.5} 
+        };
+
+        double[] result = new double[4];
+        double[][] table_result;
+
+        SimplexMethod S = new SimplexMethod(table);
+        table_result = S.calculate(result);
+        
+        System.out.println(result[0] + ":" + result[1] + ":" + result[2] + ":" + result[3]);
+        System.out.println("F = " + (result[0] * 21 + result[1] * 18 + result[2] * 16 + result[3] * 17.5));
+    }
+    
 }
--- Modules/SimplexMethod/src/com/ivo/module/SimplexMethod.java
+++ Modules/SimplexMethod/src/com/ivo/module/SimplexMethod.java
@@ -9,33 +9,121 @@ import java.util.List;
  */
 public class SimplexMethod {
 
-    // таблица без базисных переменных
+    // table without basis vars
     private double[][] table;
     
-    // размерность строки, столбца
-    private int n, m;
+    // size row, cols
+    private int m, n;
     
-    // базисы
-    private List<Integer> basis;
+    // basis
+    private final List<Integer> basis;
 
     public SimplexMethod(double[][] source) {
-        n = source.length;
         m = source.length;
+        n = source[0].length;
         
-        table = new double[n][m];
+        table = new double[m][n + m - 1];
         basis = new ArrayList<>();
         
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < m; col++) {
-                table[row][col] = source[row][col];
+        // create basis
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < table[row].length; col++) {
+                if (col < n) {
+                    table[row][col] = source[row][col];
+                } else {
+                    table[row][col] = 0;
+                }
+            }
+            // add coefficient 1 before basis var in row
+            if ((n + row) < table[row].length) {
+                table[row][n + row] = 1;
+                basis.add(n + row);
+            } 
+        }
+        
+        n = table[0].length;
+    }
+    
+    public double[][] calculate(double[] result) {
+        // main (leader) col and row
+        int mainCol, mainRow;
+        
+        while (!isItEnd()) {
+            mainCol = findMainCol();
+            mainRow = findMainRow(mainCol);
+            basis.set(mainRow, mainCol);
+            
+            double[][] newTable = new double[m][n];
+            
+            for (int j = 0; j < n; j++) {
+                newTable[mainRow][j] = table[mainRow][j] / table[mainRow][mainCol];
+            }
+            
+            for (int i = 0; i < m; i++) {
+                if (i == mainRow) {
+                    continue;
+                }
+                
+                for (int j = 0; j < n; j++) {
+                    newTable[i][j] = table[i][j] - table[i][mainCol] * newTable[mainRow][j];
+                }
+            }
+            table = newTable;
+        }
+        
+        // set in result finding values X
+        for (int i = 0; i < result.length; i++) {
+            int k = basis.indexOf(i + 1);
+            if (k != -1) {
+                result[i] = table[k][0];
+            } else {
+                result[i] = 0;
             }
         }
         
-        
+        return table;
     }
-    
-    
 
-    
+    private boolean isItEnd() {
+        boolean flag = true;
+
+        for (int j = 1; j < n; j++) {
+            if (table[m - 1][j] < 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        return flag;
+    }
+
+    private int findMainCol() {
+        int mainCol = 1;
+
+        for (int j = 2; j < n; j++) {
+            if (table[m - 1][j] < table[m - 1][mainCol]) {
+                mainCol = j;
+            }
+        }
+
+        return mainCol;
+    }
+
+    private int findMainRow(int mainCol) {
+        int mainRow = 0;
+
+        for (int i = 0; i < m - 1; i++)
+            if (table[i][mainCol] > 0)
+            {
+                mainRow = i;
+                break;
+            }
+
+        for (int i = mainRow + 1; i < m - 1; i++)
+            if ((table[i][mainCol] > 0) && ((table[i][0] / table[i][mainCol]) < (table[mainRow][0] / table[mainRow][mainCol])))
+                mainRow = i;
+
+        return mainRow;
+    }
     
 }
