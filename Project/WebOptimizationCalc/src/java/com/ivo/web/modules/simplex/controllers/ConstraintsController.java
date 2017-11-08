e38fe74990d8a25037ea7cf64501060a7d54881d
io <webworkk@mail.ru>
2017-11-08 12:54:58 +0300

    start out results simplex method

--- Project/WebOptimizationCalc/src/java/com/ivo/web/modules/simplex/controllers/ConstraintsController.java
+++ Project/WebOptimizationCalc/src/java/com/ivo/web/modules/simplex/controllers/ConstraintsController.java
@@ -4,6 +4,7 @@ package com.ivo.web.modules.simplex.controllers;
 import com.ivo.module.SimplexMethod;
 import com.ivo.web.modules.simplex.beans.ArgsBean;
 import com.ivo.web.modules.simplex.beans.FunctionBean;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
@@ -15,7 +16,7 @@ import javax.faces.bean.SessionScoped;
  */
 @ManagedBean
 @SessionScoped
-public class ConstraintsController {
+public class ConstraintsController implements Serializable {
 
     private List<Integer> argsList;
     private List<Integer> constraintsList;
@@ -32,6 +33,9 @@ public class ConstraintsController {
     private int countArgs;
     private int countConstraints;
     
+    // results
+    private StringBuilder answer = new StringBuilder();
+    
     /**
      * Creates a new instance of ConstraintsBean
      */
@@ -47,20 +51,57 @@ public class ConstraintsController {
     
     public String handleSecondStep() {
         if (!argValues.isEmpty()) {
-            Double[] values;
+            answer.append("Решим прямую задачу линейного программирования симплексным методом, "
+                    + "с использованием симплексной таблицы").append("<br/>");
+            
+            Double[] values = new Double[argValues.get(0).getValues().length];
  
+            // CREATE A IN TABLE VALUES
             double[] result = new double[countArgs];
+            // read constraints value
             // + 1 goal function
             // + 1 valueCondition
             double[][] table = new double[countConstraints + 1][countArgs + 1];
+            
+            ArgsBean argBean;
             for (int i = 0; i < argValues.size(); i++) {
+                argBean = argValues.get(i);
+                if (maxOrMin.equals("min")) {
+                    answer.append("Определим минимальное значение целевой функции F(X) = ")
+                            .append(getStringGoalFunc())
+                            .append(" при следующих условиях-ограничений.")
+                            .append(getStringConstraints());
+                    values = new Double[argBean.getValues().length];
+                    if (argBean.getCondition().equals("≤")) {
+                        for (int j = 0; j < argBean.getValues().length; j++) {
+                            values[j] = -(argBean.getValues()[j]);
+                        }
+                    } else {
+                        for (int j = 0; j < argBean.getValues().length; j++) {
+                            values[j] = argBean.getValues()[j];
+                        }
+                    }
+                }
+                if (maxOrMin.equals("max")) {
+                    values = new Double[argBean.getValues().length];
+                    if (argBean.getCondition().equals("≥")) {
+                        for (int j = 0; j < argBean.getValues().length; j++) {
+                            values[j] = -(argBean.getValues()[j]);
+                        }
+                    } else {
+                        for (int j = 0; j < argBean.getValues().length; j++) {
+                            values[j] = argBean.getValues()[j];
+                        }
+                    }
+                }
+                
                 table[i][0] = argValues.get(i).getValueCondition();
-                values = argValues.get(i).getValues();
                 for (int j = 0; j < values.length; j++) {
                     table[i][j + 1] = values[j];
                 }
             }
             
+            // read the goal func values
             int n = table.length - 1;
             table[n][0] = 0;
             for (int i = 0; i < funcValues.size(); i++) {
@@ -80,10 +121,43 @@ public class ConstraintsController {
             
             SimplexMethod s = new SimplexMethod(table);
             s.calculate(result);
-            System.out.println("F = " + (result[0] * 21 + result[1] * 18 + result[2] * 16 + result[3] * 17.5));
+            
+            double answer = 0;
+            double valueFunc = 0;
+            for (int i = 0; i < result.length; i++) {
+                answer += result[i] * funcValues.get(i).getValue();
+            }
+            
+            System.out.println("F = " + (answer + freeMemberC));
             return "step3";
         }
-        return "error_page";
+        return "error";
+    }
+    
+    private String getStringGoalFunc() {
+        StringBuilder goalFunc = new StringBuilder();
+        for (FunctionBean bean : funcValues) {
+            goalFunc.append(bean.getValue()).append("x" + bean.getNameArg()).append("+");
+        }
+        goalFunc.append(freeMemberC);
+        
+        return goalFunc.toString();
+    }
+    
+    private String getStringConstraints() {
+        StringBuilder constraints = new StringBuilder();
+        StringBuilder tmpconstraints;
+        for (ArgsBean bean : argValues) {
+            tmpconstraints = new StringBuilder();
+            Double[] values = bean.getValues();
+            for (int i = 0; i < values.length; i++) {
+                tmpconstraints.append(values[i]).append("x").append(i+1).append("+");
+            }
+            constraints.append(tmpconstraints.toString().substring(0, tmpconstraints.toString().length() - 1));
+            constraints.append(" = ").append(bean.getValueCondition()).append("\r\n");
+        }
+ 
+        return constraints.toString();
     }
 
     public int getCountArgs() {
@@ -166,5 +240,13 @@ public class ConstraintsController {
 
     public void setMaxOrMin(String maxOrMin) {
         this.maxOrMin = maxOrMin;
+    }
+
+    public StringBuilder getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(StringBuilder answer) {
+        this.answer = answer;
     }
 }
